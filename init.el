1868b45a3dcb9723f3e697cd72ff442f04d5978a
gwonk <ericm@peachisland.com>
2023-01-06 09:23:05 -0500

    Nano conversion start

--- init.el
+++ init.el
@@ -4,6 +4,21 @@
 ;; Emacs init file responsible for either loading pre-compiled configuration
 ;; file or tangling and loading a literate org configuration file
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+
+
 ;; Don't attempt to find/apply special handlers to files loading during
 ;; startup.
 (let ((file-name-handler-alist nil))
