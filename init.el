31b8d58ee58a53b1c19fb0f0aded248cdfcacb83
gwonk <ericm@peachisland.com>
2023-03-18 21:26:36 -0400

    Rust updates.

--- init.el
+++ init.el
@@ -29,3 +29,17 @@
     (org-babel-load-file (expand-file-name "readme.org" user-emacs-directory))))
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(error ((t (:inherit nano-face-critical :foreground "black"))))
+ '(flycheck-error-list-error ((t nil)))
+ '(flycheck-fringe-error ((t nil))))
